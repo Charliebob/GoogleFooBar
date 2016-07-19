6f7d914bfdd2ea50dd89dbb1b28b286597eafe33
Charliebob <CharliebobCharliebob0604@gmail.com>
2016-07-19 10:48:44 -0700

    commit

--- Google/src/backTracking/SodukuSolver.java
+++ Google/src/backTracking/SodukuSolver.java
@@ -1,5 +1,56 @@
 package backTracking;
 
 public class SodukuSolver {
-
+	public static void solveSudoku(char[][] board) {
+        if(board==null || board.length==0) return;
+        solve(board);
+    }
+    public static boolean solve(char[][] board){
+        for(int i=0; i<board.length; i++){
+            for(int j=0; j<board[0].length; j++){
+                if(board[i][j]=='.'){
+                    for(char c='1'; c<='9'; c++){
+                        if(isValid(board, i, j, c)){
+                            board[i][j] = c;
+                            if(solve(board)) return true;
+                            board[i][j] = '.'; //otherwise go back
+                        }
+                    }
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+    public static boolean isValid(char[][] board, int i, int j, char c){
+        for(int row = 0; row<9; row++){
+            if(board[row][j]==c) return false;
+        }
+        for(int col = 0; col<9; col++){
+            if(board[i][col]==c) return false;
+        }
+        for(int row=(i/3)*3; row<(i/3)*3+3; row++){ //(0,3)(3,6)(6,9)
+            for(int col=(j/3)*3; col<(j/3)*3+3; col++){
+                if(board[row][col]==c) return false;
+            }
+        }
+        return true;
+    }
+    public static void main(String[] args){
+    	char[][] board = {{'5','3','.','.','7','.','.','.','.'},
+    					  {'6','.','.','1','9','5','.','.','.'},
+    					  {'.','9','8','.','.','.','.','6','.'},
+    					  {'8','.','.','.','6','.','.','.','3'},
+    					  {'4','.','.','8','.','3','.','.','1'},
+    					  {'7','.','.','.','2','.','.','.','6'},
+    					  {'.','6','.','.','.','.','2','8','.'},
+    					  {'.','.','.','4','1','9','.','.','5'},
+    					  {'.','.','.','.','8','.','.','7','9'},
+    					 };
+    	solveSudoku(board);
+    	for(char[] i: board){
+    		System.out.println(i);
+    	}
+    }
+    
 }
